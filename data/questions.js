7343f2384a03c656dcbb8b0379bd404594b5be08
Nadir <34311347+spoutnik911@users.noreply.github.com>
2021-08-25 16:34:47 +0200

    Fix

--- data/questions.js
+++ data/questions.js
@@ -7,28 +7,36 @@ export const questions = [
     {
         "question": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis qui quisquam ex, quaerat necessitatibus eaque consectetur optio quam corporis! Natus velit delectus iusto adipisci, quia saepe ducimus dolores asperiores corporis.",
         "answer": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis qui quisquam ex, quaerat necessitatibus eaque consectetur optio quam corporis! Natus velit delectus iusto adipisci, quia saepe ducimus dolores asperiores corporis."
-    },    {
+    },    
+    {
         "question": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis qui quisquam ex, quaerat necessitatibus eaque consectetur optio quam corporis! Natus velit delectus iusto adipisci, quia saepe ducimus dolores asperiores corporis.",
         "answer": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis qui quisquam ex, quaerat necessitatibus eaque consectetur optio quam corporis! Natus velit delectus iusto adipisci, quia saepe ducimus dolores asperiores corporis."
-    },    {
+    },    
+    {
         "question": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis qui quisquam ex, quaerat necessitatibus eaque consectetur optio quam corporis! Natus velit delectus iusto adipisci, quia saepe ducimus dolores asperiores corporis.",
         "answer": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis qui quisquam ex, quaerat necessitatibus eaque consectetur optio quam corporis! Natus velit delectus iusto adipisci, quia saepe ducimus dolores asperiores corporis."
-    },    {
+    },    
+    {
         "question": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis qui quisquam ex, quaerat necessitatibus eaque consectetur optio quam corporis! Natus velit delectus iusto adipisci, quia saepe ducimus dolores asperiores corporis.",
         "answer": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis qui quisquam ex, quaerat necessitatibus eaque consectetur optio quam corporis! Natus velit delectus iusto adipisci, quia saepe ducimus dolores asperiores corporis."
-    },    {
+    },    
+    {
         "question": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis qui quisquam ex, quaerat necessitatibus eaque consectetur optio quam corporis! Natus velit delectus iusto adipisci, quia saepe ducimus dolores asperiores corporis.",
         "answer": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis qui quisquam ex, quaerat necessitatibus eaque consectetur optio quam corporis! Natus velit delectus iusto adipisci, quia saepe ducimus dolores asperiores corporis."
-    },    {
+    },    
+    {
         "question": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis qui quisquam ex, quaerat necessitatibus eaque consectetur optio quam corporis! Natus velit delectus iusto adipisci, quia saepe ducimus dolores asperiores corporis.",
         "answer": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis qui quisquam ex, quaerat necessitatibus eaque consectetur optio quam corporis! Natus velit delectus iusto adipisci, quia saepe ducimus dolores asperiores corporis."
-    },    {
+    },    
+    {
         "question": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis qui quisquam ex, quaerat necessitatibus eaque consectetur optio quam corporis! Natus velit delectus iusto adipisci, quia saepe ducimus dolores asperiores corporis.",
         "answer": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis qui quisquam ex, quaerat necessitatibus eaque consectetur optio quam corporis! Natus velit delectus iusto adipisci, quia saepe ducimus dolores asperiores corporis."
-    },    {
+    },    
+    {
         "question": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis qui quisquam ex, quaerat necessitatibus eaque consectetur optio quam corporis! Natus velit delectus iusto adipisci, quia saepe ducimus dolores asperiores corporis.",
         "answer": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis qui quisquam ex, quaerat necessitatibus eaque consectetur optio quam corporis! Natus velit delectus iusto adipisci, quia saepe ducimus dolores asperiores corporis."
-    },    {
+    },    
+    {
         "question": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis qui quisquam ex, quaerat necessitatibus eaque consectetur optio quam corporis! Natus velit delectus iusto adipisci, quia saepe ducimus dolores asperiores corporis.",
         "answer": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis qui quisquam ex, quaerat necessitatibus eaque consectetur optio quam corporis! Natus velit delectus iusto adipisci, quia saepe ducimus dolores asperiores corporis."
     }
